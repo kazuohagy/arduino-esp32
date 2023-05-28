abec299238fab029431fb305d59dd84d971a0a61
kazuohagy <kazuohagy@users.noreply.github.com>
2023-05-28 20:33:17 -0300

    esta funcionando

--- sketch_apr10a.ino
+++ sketch_apr10a.ino
@@ -1,30 +1,21 @@
 #include "FirebaseESP32.h"
-// adicionei aqui qualquer coisa tira
-//#include "Network.h"
 
-// Network network(2);
-// networl.motorOn();
-//  Fim da adição
 
 #define FIREBASE_HOST "https://gaia-25c62-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "G5HN59JZValyPF813J4zc6kzI5Zit6zOiN6C1o71"
 #define WIFI_SSID "Kazuo_EXT"
 #define WIFI_PASSWORD "12345678w"
+#define RELAY_PIN 15
 
-// Variáveis globais
 FirebaseData firebaseData; // Cria um objeto para armazenar os dados do Firebase
-// Crie um objeto FirebaseESP32 para acessar o Firestore
-FirebaseESP32 firebase;
-
 const int pinoSensorUmidade = A0;
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  pinMode(pinoSensorUmidade, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(pinoSensorUmidade,INPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // Conecta o ESP32 à sua rede Wi-Fi
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
@@ -33,76 +24,44 @@ void setup()
 }
 
 // the loop function runs over and over again forever
-void loop()
-{
+void loop() {
   delay(2000); // Aguarda 2 segundos antes de recuperar os dados do Firebase
   // Recupera a temperatura e a umidade do Firebase
 
-  int temperature = Firebase.get(firebaseData, "/planta/temperatura");
+
   int humidity = Firebase.get(firebaseData, "/planta/umidade");
-  Firebase.setString(firebaseData, "/solo", analogRead(pinoSensorUmidade));
-
-  // Gere um número aleatório
-  int random_number = random(100);
-
-  // Crie um objeto JSON com o número aleatório
-  FirebaseJson json;
-  json.add("number", random_number);
-
-  // Envie o objeto JSON para o Firestore
-  if (Firebase.setJSON(firebaseData, "/random_number", json))
-  {
-    Serial.print("Sent random number to Firestore: ");
-    Serial.println(random_number);
-  }
-  else
-  {
-    Serial.print("Failed to send random number to Firestore: ");
-    Serial.println(firebaseData.errorReason());
-  }
-
-  Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), content.raw())
-      // Faz um get da string a partir do nó /mario no Firebase
-      // if (Firebase.getString(firebaseData, "/mario")) {
-      //   String myString = firebaseData.jsonString();
-      //   Serial.print("String from Firebase: ");
-      //   Serial.println(myString);
-      //   //Firebase.setString(firebaseData,"/mario","tomanocu");
-      //
-      // } else {
-      //   Serial.println("Failed to get string from Firebase!");
-      // }
-      Serial.println(analogRead(pinoSensorUmidade));
-
-  if (Firebase.getInt(firebaseData, "/planta/umidade"))
-  {
-    if (firebaseData.dataType() == "int")
-    {
-      // intValue = firebaseData.intData();
-      Serial.print("Umidade: ");
-      // Serial.println(intValue);
+  Firebase.setString(firebaseData,"/umidade", analogRead(pinoSensorUmidade));
+  // Faz um get da string a partir do nó /mario no Firebase
+ // if (Firebase.getString(firebaseData, "/mario")) {
+ //   String myString = firebaseData.jsonString();
+ //   Serial.print("String from Firebase: ");
+ //   Serial.println(myString);
+ //   //Firebase.setString(firebaseData,"/mario","tomanocu");
+//
+ // } else {
+ //   Serial.println("Failed to get string from Firebase!");
+ // }
+ Serial.println(analogRead(pinoSensorUmidade));
+  
+    if(Firebase.getInt(firebaseData,"/planta/umidade")){
+      if(firebaseData.dataType()=="int"){
+        //intValue = firebaseData.intData();
+        Serial.print("Umidade: ");
+        //Serial.println(intValue);
+      }else{
+        Serial.println(firebaseData.errorReason());
+      }
     }
-    else
-    {
-      Serial.println(firebaseData.errorReason());
-    }
-  }
-  if (Firebase.get(firebaseData, "/solo"))
-  {
+    if (Firebase.get(firebaseData, "/mario")) {
     // Verifica o tipo de dados retornados
-    if (firebaseData.dataType() == "string")
-    {
+    if (firebaseData.dataType() == "string") {
       String myString = firebaseData.stringData();
       Serial.print("String from Firebase: ");
       Serial.println(myString);
-    }
-    else
-    {
+    } else {
       Serial.println("Data from Firebase is not a string!");
     }
-  }
-  else
-  {
+  } else {
     Serial.println("Failed to get data from Firebase!");
   }
 
